34160e83bf36fe63f4c9da2e3f8a2a897211e25f
Артур Ихсонов <artur.ixsonov.95@mail.ru>
2020-02-27 14:47:05 +0300

    Сделал 2 домашнее задание

---
+++ Урок 2/Урок 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            #region Написать метод, возвращающий минимальное из трех чисел.
+            //Console.Write("Введите число a: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Введите число b: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Введите число c: ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //int max;
+            //if (a < b && a < c)
+            //{
+            //    max = a;
+            //}
+            //else
+            //{
+            //    if (b < c)
+            //    {
+            //        max = b;
+            //    }
+            //    else
+            //    {
+            //        max = c;
+            //    }
+            //}
+
+
+
+            //Console.WriteLine($"Наименьшее число {max}");
+            //Console.ReadKey();
+            #endregion
+            #region Написать метод подсчета цифр числа
+
+            //Console.Write("Введите число : ");
+            //int num = int.Parse(Console.ReadLine());
+            //int i = 0;
+            //while (num > 0)
+            //{
+            //    i++;
+            //    num /= 10;
+            //}
+            //Console.WriteLine("Количество цифр введенного числа : {0}", i);
+            //Console.ReadKey();
+            //return 0;
+
+            #endregion
+            #region С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+           
+            #endregion
+        }
+    }
+}
